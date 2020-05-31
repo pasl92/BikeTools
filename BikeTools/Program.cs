49b44f471756994bd44b8b12819745f17c15ff8f
Paweł Sałata <pawel.salata@microsoft.wsei.edu.pl>
2020-05-31 21:07:38 +0200

    Add Road bika frame calculator

--- BikeTools/Program.cs
+++ BikeTools/Program.cs
@@ -48,7 +48,9 @@ namespace BikeTools
         {
             int wzrost;
             string[] frameSizeM = { "33-36 cm (13\" - 14\")", "35-38 cm (14\" - 15\")", "38-40 cm (15\" - 16\")", "40-43 cm (16\" - 17\")", "42-45 cm (16,5\" - 17, 5\")", "44-47 cm (17,5\" - 18, 5\")", "46-49 cm (18\" - 19\"), 49-52 cm (19\" - 20, 5\")", "51-56 cm (20,5\"-22\")", "od 56 cm (od 22\")" };
-            
+            string[] frameSizeR = { "47-49 cm", "49-51 cm", "51- 53 cm", "53-55 cm", "55-57 cm", "57-60 cm", "60- 62 cm", "62-64 cm", "od 64 cm" };
+
+
             Console.WriteLine("Podaj wzrost: ");
             wzrost = int.Parse(Console.ReadLine());
 
@@ -66,7 +68,7 @@ namespace BikeTools
                 {
                     int j = 155;
 
-                    if (wzrost >= 130 && wzrost <= 200)
+                    if (wzrost >= 150 && wzrost <= 200)
 
                     {
 
@@ -96,7 +98,40 @@ namespace BikeTools
 
                 }
 
-                else if ( b == "3")
+                else if (b == "2")
+                {
+                    int j = 160;
+
+                    if (wzrost >= 155 && wzrost <= 200)
+
+                    {
+
+                        for (int n = 0; n <= 10; n++)
+                        {
+                            if (wzrost <= j)
+                            {
+                                Console.WriteLine("\nOdpowiedni rozmiar ramy szosowej to " + frameSizeR[n]);
+
+                                n = 10;
+                            }
+
+                            else
+                            {
+                                j += 5;
+                            }
+                        }
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Aby dobrać odpowiedni rozmiar dla twojego wzrostu, polecamy udać się na konsultacje w sklepie rowerowym.");
+                    }
+
+                    Console.WriteLine("\n0 - KONIEC");
+                    Console.WriteLine("3 - Menu główne\n");
+                }
+
+                else if (b == "3")
 
                 {
                     Menu();
