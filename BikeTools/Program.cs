0dab02a2d8d3d8224f24efc89a8d22d2a98f2330
Paweł Sałata <pawel.salata@microsoft.wsei.edu.pl>
2020-05-31 18:03:02 +0200

    Menu action

--- BikeTools/Program.cs
+++ BikeTools/Program.cs
@@ -6,11 +6,12 @@ namespace BikeTools
     {
         static void KalkulatorSzprych()
         {
-
+            Console.WriteLine("In progress");
         }
 
         static void KalkulatorRam()
         {
+            Console.WriteLine("In progress");
 
         }
 
@@ -26,6 +27,33 @@ namespace BikeTools
             Console.WriteLine("\n0. KONIEC");
 
 
+            while (1 != 0)
+            {
+                string h = Console.ReadLine();
+
+                if ((h == "0") || (h == "1") || (h == "2"))
+                {
+
+                    switch (h)
+                    {
+                        case "1":
+                            KalkulatorRam();
+                            break;
+
+                        case "2":
+                            KalkulatorSzprych();
+                            break;
+
+                        case "0":
+                            System.Environment.Exit(1);
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nWprowadz poprawne dane\n0 - Exit\n1 - Kalkulator Rozmiaru Ramy \n2 - Kalkulator długości szprych");
+                }
+            }
         }
     }
 }
