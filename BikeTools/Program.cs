e6c4f9d77ba9e5b4bb8d68b2e2b9726f3b9bdaa1
Paweł Sałata <pawel.salata@microsoft.wsei.edu.pl>
2020-05-31 17:36:14 +0200

    Add menu and logo

--- BikeTools/Program.cs
+++ BikeTools/Program.cs
@@ -6,7 +6,15 @@ namespace BikeTools
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("WSEI 2020/2021\n");
+
+            Console.WriteLine("++++++++++  +++  +++  +++  ++++++++++      ++++++++++  ++++++++++   ++++++++++   +++       ++++++++++\n++      ++  +++  +++ +++   ++                  ++      ++      ++   ++      ++   +++       ++        \n++      ++  +++  ++++++    ++                  ++      ++      ++   ++      ++   +++       ++        \n++++++++++  +++  ++++      ++++++++++          ++      ++      ++   ++      ++   +++       ++++++++++\n++      ++  +++  ++++++    ++                  ++      ++      ++   ++      ++   +++               ++\n++      ++  +++  +++ +++   ++                  ++      ++      ++   ++      ++   +++               ++\n++++++++++  +++  +++  +++  ++++++++++          ++      ++++++++++   ++++++++++   +++++++   ++++++++++\n");
+
+            Console.WriteLine("1. Kalkulator Rozmiaru Ramy");
+            Console.WriteLine("2. Kalkulator długości szprych");
+            Console.WriteLine("3. INFO");
+            Console.WriteLine("\n0. KONIEC");
+
         }
     }
 }
