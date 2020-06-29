a131eb3ebfb0083c8e7642f4e6a47c336b2c5ffc
Paweł Sałata <pawel.salata@microsoft.wsei.edu.pl>
2020-06-29 20:21:44 +0200

    Add BMI calculator

--- BikeToolsLib/BikeToolsMenu.cs
+++ BikeToolsLib/BikeToolsMenu.cs
@@ -11,14 +11,15 @@ namespace BikeToolsLib
         {
             Console.WriteLine("\n1. Kalkulator rozmiaru ramy");
             Console.WriteLine("2. Kalkulator czasu jazdy Ebike");
-            Console.WriteLine("3. Prezent urodzinowy");
-            Console.WriteLine("4. Color mode");
-            Console.WriteLine("5. INFO");
+            Console.WriteLine("3. Kalkulator BMI");
+            Console.WriteLine("4. Prezent urodzinowy");
+            Console.WriteLine("5. Color mode");
+            Console.WriteLine("6. INFO");
             Console.WriteLine("\n0. KONIEC\n");
 
             string h = "";
 
-            while ((h != "0") && (h != "1") && (h != "2") && (h != "3") && (h != "4") && (h != "5"))
+            while ((h != "0") && (h != "1") && (h != "2") && (h != "3") && (h != "4") && (h != "5") && (h != "6"))
             {
                 h = Console.ReadLine();
             }
@@ -37,15 +38,19 @@ namespace BikeToolsLib
                     break;
 
                 case "3":
+                    KalkulatorBMI.BMI();
+                    break;
+
+                case "4":
                     Sounds.SupriseSound();
                     BikeToolsMenu.Menu();
                     break;
 
-                case "4":
+                case "5":
                     ColorMode.ColorMenu();
                     break;
 
-                case "5":
+                case "6":
                     BikeToolsInfo.Info();
                     break;
 
---
+++ BikeToolsLib/KalkulatorBMI.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace BikeToolsLib
+{
+    class KalkulatorBMI
+    {
+        public static void BMI()
+        {
+
+            Console.WriteLine("\nPodaj wagę: ");
+            if (double.TryParse(Console.ReadLine(), out double w))
+            {
+                Console.WriteLine("\nPodaj wzrost ");
+                if (double.TryParse(Console.ReadLine(), out double h))
+                {
+                    BMIlogic(w, h, out double u);
+                    Console.WriteLine("\nTwoje BMI to: " + string.Format("{0:.0}", u));
+
+                    if (u < 16)
+                    {
+                        Console.WriteLine("Waga zdecydowanie za niska");
+                    }
+                    else if (u < 18.5)
+                    {
+                        Console.WriteLine("Waga za nisaka");
+                    }
+                    else if (u < 25)
+                    {
+                        Console.WriteLine("Waga w normie");
+                    }
+                    else if (u < 30)
+                    {
+                        Console.WriteLine("Waga za wysoka");
+                    }
+                    else if (u < 35)
+                    {
+                        Console.WriteLine("Waga zdecydowanie za wysoka");
+                    }
+
+                    Console.WriteLine();
+
+                }
+                else
+                {
+                    BMI();
+                }
+
+            }
+            else
+            {
+                BMI();
+            }
+
+            BikeToolsMenu.Menu();
+
+        }
+
+        public static void BMIlogic(double w, double h, out double u)
+        {
+            u = w / ((h * 0.01) * (h * 0.01));
+        }
+
+
+    }
+}
